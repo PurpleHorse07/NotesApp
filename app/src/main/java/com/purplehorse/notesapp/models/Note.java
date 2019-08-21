cbdeb9e8d0cd0fc36fb8600ab648072696efe26b
Sayantan <Sayantan>
2019-08-21 16:05:25 +0530

    First Commit

---
+++ app/src/main/java/com/purplehorse/notesapp/models/Note.java
@@ -0,0 +1,17 @@
+package com.purplehorse.notesapp.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Note {
+
+    String title;
+    String content;
+    Long timestamp;
+}
